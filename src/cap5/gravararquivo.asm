106641dcbfa4c4e9b1a5bf7b9386510f82dea6a2
Fábio Marques <fmarques@fmarques.eti.br>
2026-03-16 15:45:37 -0300

    Refactor Write File exercise
    - centralize file operation constants and update file write implementation
    	- refactor bibliotecaE.inc library file
    	- refactor gravararquivo.asm source file

--- src/cap5/gravararquivo.asm
+++ src/cap5/gravararquivo.asm
@@ -1,7 +1,11 @@
+%include 'bibliotecaE.inc'
+
 section .data
-         nomArquivo db 'arquivo.txt', 0x0
-         conteudo db 'Hello World 2!', 0xA
-         mens_erro db 'Erro ao abrir o arquivo!', 0x0
+         n_arq db 'arquivo.txt', 0x0
+         msg   db 'Hello World 2!', 0xA
+         t_msg equ $-msg
+         mse   db 'Erro ao abrir o arquivo!', 0x0
+         t_mse equ $-mse
 
 section .text
 
@@ -9,9 +13,9 @@ global _start
 
 _start:
          ; Abrir o arquivo
-         mov   rax, 0x2
-         mov   rdi, nomArquivo
-         mov   rsi, 0x2 | 0x40 | 0x80
+         mov   rax, OPEN_FILE
+         mov   rdi, n_arq
+         mov   rsi, O_RDWR | O_CREATE | O_TRUNC
          mov   rdx, 0o664           ; Perms: 6-owner, 6-group, 4-others
          syscall
 
@@ -20,35 +24,35 @@ _start:
 
 continuar:
          mov   rdi, rax
-         mov   rax, 0x1
-         mov   rsi, conteudo
-         mov   rdx, 15
+         mov   rax, SYS_WRITE
+         mov   rsi, msg
+         mov   rdx, t_msg
          syscall
 
-         mov   rax, 0x3
+         mov   rax, CLOSE_FILE
          mov   rdi, [rsp]
          syscall
          jmp   finalizar
 
 erroExiste:
-         mov   rax, 0x2
-         mov   rdi, nomArquivo
-         mov   rsi, 0x2 | 0x8 | 0x80
+         mov   rax, OPEN_FILE
+         mov   rdi, n_arq
+         mov   rsi, O_RDWR | O_APPEND | O_TRUNC
          mov   rdx, 0o664           ; Perms: 6-owner, 6-group, 4-others
          syscall
 
-         cmp rax, 0x0
-         jl erro
+         cmp rax, 0x0               ; Update processor flags (RFLAGS)
+         jl erro                    ; SF≠OF => rax < 0  (signed)
          jmp continuar
 
 erro:
-         mov   rax, 0x1
-         mov   rdi, 0x1
-         mov   rsi, mens_erro
-         mov   rdx, 25
+         mov   rax, SYS_WRITE
+         mov   rdi, STDOUT
+         mov   rsi, mse
+         mov   rdx, t_mse
          syscall
 
 finalizar:
-         mov   rax, 0x3C
-         xor   rdi, rdi
+         mov   rax, SYS_EXIT
+         mov   rdi, EXIT_SUCESS
          syscall
